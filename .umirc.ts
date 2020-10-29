84141c18c9e9d619ece0859fd7114deaf29de157
bangbang6 <577174667@qq.com>
2020-10-29 16:59:46 +0800

    aa

---
+++ .umirc.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    { path: '/', component: '@/pages/index' },
+    { path: '/cart', component: '@/pages/cart/index' },
+    { path: '/olist', component: '@/pages/olist/index' },
+    { path: '/user', component: '@/pages/user/index' },
+    { path: '/login', component: '@/pages/login/index' },
+  ],
+});
